f550d9c4e80447f67b482c347fb122298127f7f4
Alexey L <20566780+alexey-ledenev@users.noreply.github.com>
2020-09-10 12:16:32 +0000

    add react-transition-group lib

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Transition } from 'react-transition-group'
 import Logo from '../../svg/logo'
 import BurgerMenuSvg from '../../svg/menu'
 import CloseSvg from '../../svg/close'
@@ -29,7 +30,7 @@ export const Header: React.FC = () => {
         <Logo className="app-header__logo-icon" />
       </Link>
       {renderButton(<span>En</span>)}
-      {menuOpened && renderMenu()}
+      {renderMenu(menuOpened)}
     </header>
   )
 }
@@ -45,57 +46,65 @@ function renderButton(child: JSX.Element, onClick?: () => void) {
   )
 }
 
-function renderMenu() {
+function renderMenu(opened: boolean) {
   return (
-    <div className="flex items-center justify-center bg-white app-header__menu-box">
-      <nav>
-        {menuItems.map(itm => (
-          <a
-            key={itm.title}
-            href={itm.path}
-            title={itm.title}
-            className="h3 app-header__menu-item"
-          >
-            {itm.title}
-          </a>
-        ))}
-        {menuSubitems.map(itm => (
-          <a
-            key={itm.title}
-            href={itm.path}
-            title={itm.title}
-            className="txt2 app-header__menu-item_small"
-          >
-            {itm.title}
-          </a>
-        ))}
-      </nav>
-      <div>
-        <h3 className="h3">Контакты</h3>
-        <p className="txt2">GREENFIELD TEA Ltd., London, W1U 2HQ, UK</p>
-        <div>
-          <p className="txt2">
-            Email:&nbsp;
-            <a href="mailto:yourmail@greenfield.uk">yourmail@greenfield.uk</a>
-          </p>
-          <p className="txt2">
-            Phone:&nbsp;
-            <a href="tel:+735687359868">+7 (356) 873 598 68</a>
-          </p>
-          <p className="txt2">
-            Fax:&nbsp;
-            <a href="fax:+735676594534">+7 (356) 765 945 34</a>
-          </p>
-        </div>
-        <div>
-          <h5 className="h5">Мы в социальных сетях</h5>
-          <div className="flex">
-            <a href="#">twit</a>
-            <a href="#">inst</a>
-            <a href="#">vk</a>
+    <Transition in={opened} timeout={500}>
+      {state => (
+        <div
+          className={`flex items-center justify-center bg-white app-header__menu-box ${state}`}
+        >
+          <nav>
+            {menuItems.map(itm => (
+              <a
+                key={itm.title}
+                href={itm.path}
+                title={itm.title}
+                className="h3 app-header__menu-item"
+              >
+                {itm.title}
+              </a>
+            ))}
+            {menuSubitems.map(itm => (
+              <a
+                key={itm.title}
+                href={itm.path}
+                title={itm.title}
+                className="txt2 app-header__menu-item_small"
+              >
+                {itm.title}
+              </a>
+            ))}
+          </nav>
+          <div>
+            <h3 className="h3">Контакты</h3>
+            <p className="txt2">GREENFIELD TEA Ltd., London, W1U 2HQ, UK</p>
+            <div>
+              <p className="txt2">
+                Email:&nbsp;
+                <a href="mailto:yourmail@greenfield.uk">
+                  yourmail@greenfield.uk
+                </a>
+              </p>
+              <p className="txt2">
+                Phone:&nbsp;
+                <a href="tel:+735687359868">+7 (356) 873 598 68</a>
+              </p>
+              <p className="txt2">
+                Fax:&nbsp;
+                <a href="fax:+735676594534">+7 (356) 765 945 34</a>
+              </p>
+            </div>
+            <div>
+              <h5 className="h5">Мы в социальных сетях</h5>
+              <div className="flex">
+                <a href="#">twit</a>
+                <a href="#">inst</a>
+                <a href="#">vk</a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Transition>
   )
 }
