3cc4753913db82b65d8ce4a3df53706d8c4a39cd
Alexey L <20566780+alexey-ledenev@users.noreply.github.com>
2020-09-10 21:06:36 +0000

    first screen animations

--- src/components/main-screen/main-screen.tsx
+++ src/components/main-screen/main-screen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import Img from 'gatsby-image/withIEPolyfill'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import s from './main-screen.module.sass'
 
 export const MainScreen: React.FC = () => {
@@ -16,43 +17,60 @@ export const MainScreen: React.FC = () => {
       }
       fogRight: file(relativePath: { eq: "01_1_foggy.png" }) {
         childImageSharp {
-          fluid(maxWidth: 1920, quality: 80) {
-            ...GatsbyImageSharpFluid_withWebp
-            ...GatsbyImageSharpFluidLimitPresentationSize
+          fixed(width: 1920, quality: 80) {
+            ...GatsbyImageSharpFixed_withWebp
           }
         }
       }
       fogCenter: file(relativePath: { eq: "01_foggy.png" }) {
         childImageSharp {
-          fluid(maxWidth: 1920, quality: 80) {
-            ...GatsbyImageSharpFluid_withWebp
-            ...GatsbyImageSharpFluidLimitPresentationSize
+          fixed(width: 1920, quality: 80) {
+            ...GatsbyImageSharpFixed_withWebp
           }
         }
       }
     }
   `)
+  const [ready, setReady] = React.useState(false)
   return (
-    <section className="screen max-w-screen-xxl mx-auto">
-      <div className="absolute inset-0 w-full h-full">
-        <Img fluid={images.bgImage.childImageSharp.fluid} />
-      </div>
-      <div className="absolute inset-0 w-full h-full fade-in animate-2s">
-        <Img fluid={images.fogRight.childImageSharp.fluid} />
-      </div>
-      <div className="absolute inset-0 w-full h-full fade-in animate-2s">
-        <Img fluid={images.fogCenter.childImageSharp.fluid} />
-      </div>
-      <div
-        className={`relative h-full flex flex-col justify-center text-white ${s.text}`}
-      >
-        <h1 className={`text-white ${s.title}`}>
-          Обретая гармонию. Обретая себя.
-        </h1>
-        <h2 className={`text-white ${s.subtitle}`}>
-          Погрузитесь в философию Greenfield
-        </h2>
-      </div>
-    </section>
+    <CSSTransition
+      in={true}
+      timeout={300}
+      appear
+      classNames="slide-right"
+      onEntered={() => setReady(true)}
+    >
+      <section className="screen">
+        <div className="relative mx-auto">
+          <Img fluid={images.bgImage.childImageSharp.fluid} />
+          <CSSTransition in={ready} timeout={2000} classNames="fade-slow">
+            <Img
+              fixed={images.fogRight.childImageSharp.fixed}
+              objectFit="cover"
+              objectPosition="50% 50%"
+              style={{ position: 'absolute', width: '100%', height: '100%' }}
+              className="inset-0"
+            />
+          </CSSTransition>
+          <Img
+            fixed={images.fogCenter.childImageSharp.fixed}
+            objectFit="cover"
+            objectPosition="50% 50%"
+            style={{ position: 'absolute', width: '100%', height: '100%' }}
+            className="inset-0"
+          />
+          <div
+            className={`absolute inset-0 w-full h-full flex flex-col justify-center text-white ${s.text}`}
+          >
+            <h1 className={`text-white ${s.title}`}>
+              Обретая гармонию. Обретая себя.
+            </h1>
+            <h2 className={`text-white ${s.subtitle}`}>
+              Погрузитесь в философию Greenfield
+            </h2>
+          </div>
+        </div>
+      </section>
+    </CSSTransition>
   )
 }
